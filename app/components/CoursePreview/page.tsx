27adfdffdadebaf7e506a87f2e299469bfe1031a
Kiran Kumar <102020044+CodingKiran@users.noreply.github.com>
2025-04-20 00:37:22 +0530

    All errors and deployment issues fixed - final

--- app/components/CoursePreview/page.tsx
+++ app/components/CoursePreview/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LessonCard from "../LessonCard/page";
 
 const CoursePreview = () => {
   return (
@@ -16,6 +17,7 @@ const CoursePreview = () => {
             costing your SaaS business growth and revenue.
           </p>
         </div>
+        <LessonCard />
       </div>
     </section>
   );
